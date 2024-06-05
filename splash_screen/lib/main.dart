79817bed0d2410415c04884dc9b748adc2d8c5d3
typescript-team <type.script@email.com>
2024-06-05 21:43:15 +0900

    commit

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -20,16 +20,17 @@ class SplashScreen extends StatelessWidget {
       ),
       home: Scaffold(
         body: Container(
-          /**
+            /**
              * Container는 decoration이라는 네임드 파라미터를 제공한다.
              * decoration이라는 매개변수는 BoxDecoration 클래스를 사용하게 되는데
              * BoxDecoration의 매개변수를 통해서 배경색, 테두리 색상, 테두리 두께 등
              * 컨테이너의 여러가지 UI 요소를 적용할 수 있다.
              */
-          decoration: const BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Center(
+            decoration: const BoxDecoration(
+              color: Colors.orange, // Color(0xFFF99231)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               /**
              * Image 위젯의 5가지 생성자
              * 
@@ -40,8 +41,23 @@ class SplashScreen extends StatelessWidget {
              * 4. Image.file 생성자 :: 파일을 통해서 이미지를 그린다.
              * 5. Image.memory 생성자 :: 메모리에서 직접 이미지를 그린다.
              */
-              child: Image.asset('assets/icon_catdog.png')),
-        ),
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/Cat-Dog.png',
+                      width: 200,
+                    ),
+                    const CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(
+                        Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
       ),
     );
   }
